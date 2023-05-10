8bd0cde3db02ab6d61c611ea55d43cb18888510d
serZahs <32044517+serZahs@users.noreply.github.com>
2023-05-10 03:17:49 -0400

    A little backend refactor

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -6,85 +8,103 @@ const io = require('socket.io')(http);
 const fs = require('fs');
 const events = require('../src/events');
 
-var clients = []; 
-
-var usersComplete = 0;
-
-let themes;
-fs.readFile(path.join(__dirname, './themes.json'), 'utf-8', (error, data) => {
-    if(error) {
-        console.log("Error loading themes.json");
-    } else {
-        themes = JSON.parse(data);
+class Client {
+    constructor(id, name, story, points) {
+        this.id = id;
+        this.name = name;
+        this.story = story;
+        this.points = points;
     }
-});
+}
 
-app.use(express.static(path.join(__dirname, '../build')));
+function HandleConnections() {
+    let clients = [];
+    let usersComplete = 0;
 
-app.get('/', function(req, res) {
-    res.send("<h1>If you've reached this page it means something broke!</h1>");
-});
+    io.on(events.ServerEvents.CONNECTION, (socket) => {
+        socket.on(events.ClientEvents.DISCONNECT, () => {
+            console.log(`${socket.id} disconnected.`);
+            //Remove from list of clients
+            let index = FindByProperty(clients, 'id', socket.id);
+            clients.splice(index, 1);
+            io.emit(events.ServerEvents.UPDATE_USERS, clients);
+        });
 
-io.on(events.ServerEvents.CONNECTION, (socket) => {
-    socket.on(events.ClientEvents.DISCONNECT, () => {
-        console.log('The user at ' + socket.id + ' disconnected.');
-        //Remove from list of clients
-        let index = findWithAttr(clients, 'id', socket.id);
-        clients.splice(index, 1);
+        console.log(`${socket.id} connected.`);
         io.emit(events.ServerEvents.UPDATE_USERS, clients);
-    });
+        clients.push(new Client(
+            socket.id,
+            null,
+            null,
+            0
+        ));
+        
+        socket.on(events.ClientEvents.GAME_START, () => {
+            if (clients.length != 1) {
+                io.emit(events.ServerEvents.WAKE_UP);
+                io.emit(events.ServerEvents.SEND_THEME, themes[Math.floor(Math.random()*themes.length)]);
+            }
+        });
 
-    console.log('A user at ' + socket.id + ' connected.');
-    io.emit(events.ServerEvents.UPDATE_USERS, clients);
-    clients.push({
-        id: socket.id,
-        name: null,
-        story: null,
-        points: 0,
+        socket.on(events.ClientEvents.NAME, (name) => {
+            let index = FindByProperty(clients, 'id', socket.id);
+            clients[index].name = name;
+            io.emit(events.ServerEvents.UPDATE_USERS, clients);
+        });
+
+        socket.on(events.ClientEvents.STORY, (story) => {
+            let index = FindByProperty(clients, 'id', socket.id);
+            clients[index].story = story;
+            usersComplete+=1;
+            const namedClients = GetNamedClients(clients);
+            if (usersComplete==namedClients.length) {
+                io.emit(events.ServerEvents.STORY_COMPLETE, clients);
+                usersComplete = 0;
+            }
+        });
+
+        socket.on(events.ClientEvents.POINTS, id => {
+            let index = FindByProperty(clients, 'id', id);
+            clients[index].points += 1;
+            io.emit(events.ServerEvents.UPDATE_USERS, clients);
+        });
+    });
+}
+
+function Main() {
+    const static_path = path.join(__dirname, '../build');
+    const themes_path = path.join(__dirname, './themes.json');
+
+    let themes = [];
+    fs.readFile(themes_path, 'utf-8', (error, data) => {
+        if(error) {
+            throw new Error("Could not load themes.json");
+        } else {
+            themes = JSON.parse(data);
+        }
     });
     
-    socket.on(events.ClientEvents.GAME_START, () => {
-        if (clients.length != 1) {
-            io.emit(events.ServerEvents.WAKE_UP);
-            io.emit(events.ServerEvents.SEND_THEME, themes[Math.floor(Math.random()*themes.length)]);
-        }
+    app.use(express.static(static_path));
+
+    app.get('/', function(req, res) {
+        res.send("<h1>If you've reached this page it means something broke!</h1>");
     });
 
-    socket.on(events.ClientEvents.NAME, (name) => {
-        let index = findWithAttr(clients, 'id', socket.id);
-        clients[index].name = name;
-        io.emit(events.ServerEvents.UPDATE_USERS, clients);
+    http.listen(5000, function() {
+        console.log('The server is now listening.');
     });
 
-    socket.on(events.ClientEvents.STORY, (story) => {
-        let index = findWithAttr(clients, 'id', socket.id);
-        clients[index].story = story;
-        usersComplete+=1;
-        const namedClients = getNamedClients(clients);
-        if (usersComplete==namedClients.length) {
-            io.emit(events.ServerEvents.STORY_COMPLETE, clients);
-            usersComplete = 0;
-        }
-    });
+    HandleConnections();
+}
 
-    socket.on(events.ClientEvents.POINTS, id => {
-        let index = findWithAttr(clients, 'id', id);
-        clients[index].points += 1;
-        io.emit(events.ServerEvents.UPDATE_USERS, clients);
-    });
-});
+Main();
 
-http.listen(5000, function() {
-    console.log('The server is now listening.');
-});
-
-function getNamedClients(clients) {
+function GetNamedClients(clients) {
     return clients.filter(c => c.name !== null);
 }
 
-// https://stackoverflow.com/questions/7176908/how-to-get-index-of-object-by-its-property-in-javascript/54015295
-const findWithAttr = (array, attr, value) => {
-    for(var i = 0; i < array.length; i += 1) {
+function FindByProperty(array, attr, value) {
+    for(let i = 0; i < array.length; i++) {
         if(array[i][attr] === value) {
             return i;
         }
